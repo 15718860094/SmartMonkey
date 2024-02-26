120204e246d70b1bd23e4e957d9b6f76d574a8e7
加州大马猴 <15718860094@163.com>
2024-02-26 22:00:35 +0800

    增加相似图片去重功能

--- settings.py
+++ settings.py
@@ -14,6 +14,11 @@ persistent=True # 是否永久保存截屏
 cache_path='data/screen_cache' # 截屏图片缓存路径
 shotcut_step=10 # 每次截屏的间隔时间
 
+# ImageHandler
+deduplication_threshold = 0.75  # 重复数据删除阈值
+
+ 
+
 # Log
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
