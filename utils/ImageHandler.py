c46348ef1e9bce1cd1d3c57d9846b5b0dfd514d1
加州大马猴 <15718860094@163.com>
2024-02-26 21:34:14 +0800

    增加图片比对功能

--- utils/ImageHandler.py
+++ utils/ImageHandler.py
@@ -10,3 +10,21 @@ class ImageHandler(object):
         with open(self.image_path, "rb") as image_file:
             encoded_image = base64.b64encode(image_file.read())
         return encoded_image.decode("utf-8")
+    
+    def calculate_mse(image1, image2):
+        img1 = cv2.imread(image1)
+        img2 = cv2.imread(image2)
+
+        if img1.shape == img2.shape:
+            err = np.sum((img1.astype("float") - img2.astype("float")) ** 2)
+            err /= float(img1.shape[0] * img1.shape[1])
+
+            return err
+        else:
+            return None
+
+image1_path = "image1.jpg"
+image2_path = "image2.jpg"
+
+mse = calculate_mse(image1_path, image2_path)
+print("Mean Squared Error (MSE):", mse)
